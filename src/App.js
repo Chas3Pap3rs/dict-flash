e0caf6ac426061d6e37d1aa7913f44f15bb1eb37
Chas3Pap3rs <futurefineants@gmail.com>
2024-04-06 14:12:19 -0400

    Initial export/upload attempt 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactinuranus
         </a>
       </header>
     </div>
